6eefe9f50661e437b466d5958285ed1f0d66e3e3
Charlie Jee & Hoon Kang <pair+charlie+hoon@balmbees.com>
2013-11-27 09:49:11 +0900

    Fix move tmp folder script

--- vingle_image/recipes/default.rb
+++ vingle_image/recipes/default.rb
@@ -1,11 +1,7 @@
 node[:deploy].each do |application, deploy|
   execute "move /tmp folder" do
-    command "mv /tmp /mnt/tmp"
-    action :nothing
-  end
-
-  execute "link /mnt/tmp folder" do
-    command "ln -s /mnt/tmp /tmp"
-    action :nothing
+    command "mv /tmp /mnt/tmp; ln -s /mnt/tmp /tmp"
+    action :run
+    not_if { ::File.exists?("/mnt/tmp")}
   end
 end
