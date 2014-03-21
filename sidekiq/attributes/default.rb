3c60c1b1db615be3a3120c12ee464813195b28e1
Charlie Jee & Hoon Kang <pair+charlie+hoon@balmbees.com>
2014-03-21 18:55:39 +0900

    Decrease mobile notification worker count.

--- sidekiq/attributes/default.rb
+++ sidekiq/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:sidekiq][:default_workers_count] = 30
 default[:sidekiq][:newsfeed_workers_count] = 4
 default[:sidekiq][:email_workers_count] = 5
-default[:sidekiq][:mobile_workers_count] = 5
+default[:sidekiq][:mobile_workers_count] = 3
 
