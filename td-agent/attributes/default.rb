8c15c9bef0ff15b4a1c61ab54080b87631e72deb
rabierre <rabierre@gmail.com>
2014-05-29 14:52:25 +0900

    Add td-agent cookbook
    Add apt cookbook
    
    Fix td-agent cookbook
    
    Fix redshift gem plugin

---
+++ td-agent/attributes/default.rb
@@ -0,0 +1,9 @@
+default[:td_agent][:api_key] = ''
+
+# default[:td_agent][:plugins] = [ { "redshift" => { "package_name" => "fluent-plugin-redshift"} }]
+default[:td_agent][:plugins] = []
+
+default[:td_agent][:includes] = false
+default[:td_agent][:default_config] = true
+default[:td_agent][:version] = '1.1.19'
+default[:td_agent][:pinning_version] = false
