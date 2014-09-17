48ab7dcc12fe226a105044c3b4a3e209ef1cb6c5
flrngel <flrngel@gmail.com>
2014-09-17 13:14:57 +0900

    eliminate elastic code from logstash

--- logstash/attributes/default.rb
+++ logstash/attributes/default.rb
@@ -1,5 +1,3 @@
-set[:logstash][:instance][:server][:elasticsearch_ip] = OpsWorksUtils::Helpers::layer_elb(node, 'elasticsearch')
-set[:logstash][:instance][:server][:elasticsearch_cluster] = 'logstash'
 set[:logstash][:instance][:server][:enable_embedded_es] = false
 set[:logstash][:instance][:server][:config_templates_cookbook] = 'opsworks_logstash'
 set[:logstash][:instance][:server][:config_templates] = {
