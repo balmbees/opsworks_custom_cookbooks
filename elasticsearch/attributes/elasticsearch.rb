c3a3d46e708e63e09a3836aaa13faed24d0c4e63
flrngel <flrngel@gmail.com>
2014-11-24 11:05:05 +0900

    increase shard, set memory limit

--- elasticsearch/attributes/elasticsearch.rb
+++ elasticsearch/attributes/elasticsearch.rb
@@ -2,10 +2,11 @@ default[:elasticsearch][:deb_url] = "https://download.elasticsearch.org/elastics
 default[:elasticsearch][:deb_sha] = "156a38c5a829e5002ae8147c6cac20effe6cd065"
 
 default[:elasticsearch][:setting] = {
-  "index.number_of_shards" => "1",
+  "index.number_of_shards" => "3",
   "index.number_of_replicas" => "0",
   "path.conf" => "/etc/elasticsearch",
   "path.data" => "/elb/data",
   "path.logs" => "/var/log/elasticsearch",
-  "script.disable_dynamic" => "false"
+  "script.disable_dynamic" => "false",
+  "indices.fielddata.cache.size" => "40%"
 }
