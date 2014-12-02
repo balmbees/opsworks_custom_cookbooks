18f39c26d6673840f24e44bfc6932241c99405a0
flrngel <flrngel@gmail.com>
2014-12-02 11:22:06 +0900

    template update (elasticsearch & logstash)

--- elasticsearch/recipes/setting.rb
+++ elasticsearch/recipes/setting.rb
@@ -1,5 +1,4 @@
 template "/etc/default/elasticsearch" do
   source "setting.erb"
-  variables( :config => node[:elasticsesarch][:setting] )
   notifies :restart, "service[elasticsesarch]"
 end
--- logstash/recipes/setting.rb
+++ logstash/recipes/setting.rb
@@ -1,5 +1,4 @@
 template "/etc/default/logstash" do
   source "setting.erb"
-  variables( :config => node[:logstash][:setting] )
   notifies :restart, "service[logstash]"
 end
