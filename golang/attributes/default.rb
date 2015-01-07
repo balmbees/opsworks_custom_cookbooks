014075edeca0e347a8dacd3a66b81078c901b8dd
flrngel <flrngel@gmail.com>
2015-01-07 18:12:40 +0900

    fix

--- golang/attributes/default.rb
+++ golang/attributes/default.rb
@@ -2,9 +2,9 @@ default['go']['version'] = '1.4'
 default['go']['platform'] = node['kernel']['machine'] =~ /i.86/ ? '386' : 'amd64'
 default['go']['filename'] = "go#{node['go']['version']}.#{node['os']}-#{node['go']['platform']}.tar.gz"
 default['go']['url'] = "http://golang.org/dl/#{node['go']['filename']}"
-default['go']['install_dir'] = '/opt'
+default['go']['install_dir'] = '/usr/local'
 default['go']['gopath'] = '/opt/go'
-default['go']['gobin'] = '/opt/go/bin'
+default['go']['gobin'] = '/usr/local/bin'
 default['go']['scm'] = true
 default['go']['packages'] = []
 default['go']['owner'] = 'root'
