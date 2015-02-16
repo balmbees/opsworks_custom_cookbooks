80f5d42131b19784366ec2ef70258c286c6825f2
Charlie Jee <charlie@vingle.net>
2015-02-16 22:20:18 +0900

    [#88488982] Faster deployment using Docker.

---
+++ docker/recipes/docker-deploy.rb
@@ -0,0 +1,95 @@
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  # if node[:opsworks][:instance][:layers].first != deploy[:environment_variables][:layer]
+  #   Chef::Log.debug("Skipping deploy::docker application #{application} as it is not deployed to this layer")
+  #   next
+  # end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
+  Chef::Log.info("docker login")
+  bash "docker login" do
+    user "root"
+    cwd "#{deploy[:deploy_to]}"
+    code <<-EOH
+      docker login -e #{node[:custom_env][:vingle][:DOCKER_EMAIL]} -u #{node[:custom_env][:vingle][:DOCKER_USERNAME]} -p #{node[:custom_env][:vingle][:DOCKER_PASSWORD]}
+    EOH
+  end
+
+  Chef::Log.info("docker cleanup")
+  bash "docker-cleanup" do
+    user "root"
+    code <<-EOH
+      if docker ps -a | grep #{deploy[:application]};
+      then
+        docker stop #{deploy[:application]}
+        sleep 3
+        docker rm #{deploy[:application]}
+        sleep 3
+      fi
+      if docker images | grep #{deploy[:application]};
+      then
+        docker rmi #{deploy[:application]}/balmbees:#{node[:custom_env][:vingle][:RAILS_ENV]}
+      fi
+    EOH
+  end
+
+  Chef::Log.info("docker pull images")
+  bash "docker pull images" do
+    user "root"
+    cwd "#{deploy[:deploy_to]}"
+    code <<-EOH
+      if docker ps -a | grep td_agent;
+      then
+        :
+      else
+        docker pull #{deploy[:application]}/dockerfiles:td_agent
+      fi
+      if docker ps -a | grep logstash;
+      then
+        :
+      else
+        docker pull #{deploy[:application]}/dockerfiles:logstash
+      fi
+      docker pull #{deploy[:application]}/balmbees:#{node[:custom_env][:vingle][:RAILS_ENV]}
+    EOH
+  end
+
+  dockerenvs = " "
+  node[:custom_env][:vingle].each do |key, value|
+    dockerenvs=dockerenvs+" -e \"#{key}=#{value}\""
+  end
+
+  Chef::Log.info("docker run #{dockerenvs} --name td -d #{deploy[:application]}/dockerfiles:td_agent")
+  Chef::Log.info("docker run -v /var/log/nginx:/var/log/nginx -d #{deploy[:application]}/dockerfiles:logstash")
+  Chef::Log.info("docker run #{dockerenvs} -v /var/log/nginx:/var/log/nginx -p 80:80 -p 8080:8080 --link td:td --name #{deploy[:application]} -d #{deploy[:application]}/balmbees:#{node[:custom_env][:vingle][:RAILS_ENV]}")
+  bash "docker-run" do
+    user "root"
+    cwd "#{deploy[:deploy_to]}"
+    code <<-EOH
+      if docker ps | grep td_agent;
+      then
+        :
+      else
+        docker run #{dockerenvs} --name td -d #{deploy[:application]}/dockerfiles:td_agent
+      fi
+      if docker ps | grep logstash;
+      then
+        :
+      else
+        docker run -v /var/log/nginx:/var/log/nginx -d #{deploy[:application]}/dockerfiles:logstash
+      fi
+      docker run #{dockerenvs} -v /var/log/nginx:/var/log/nginx -p 80:80 -p 8080:8080 --link td:td --name #{deploy[:application]} -d #{deploy[:application]}/balmbees:#{node[:custom_env][:vingle][:RAILS_ENV]}
+    EOH
+  end
+end
