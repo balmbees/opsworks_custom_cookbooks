1ffa0ec333f949f7a3c2cfdd8662bb052959de92
flrngel <flrngel@gmail.com>
2015-02-16 11:41:51 +0900

    add config dir

--- rediss3/recipes/default.rb
+++ rediss3/recipes/default.rb
@@ -11,6 +11,6 @@ include_recipe 'cron'
 cron_d 'redis-cron' do
   minute  "0"
   hour    "*/4"
-  command "redis-cli save && s3cmd put #{node[:rediss3][:rdb_path]}/#{node[:rediss3][:rdb_filename]} s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}"
+  command "redis-cli save && s3cmd --config /root/.s3cfg put #{node[:rediss3][:rdb_path]}/#{node[:rediss3][:rdb_filename]} s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}"
   user    "root"
 end
