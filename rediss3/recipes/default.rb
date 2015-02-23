b095aa862577e2fc1abf350def1905bfa8915408
flrngel <flrngel@gmail.com>
2015-02-23 19:11:08 +0900

    Revert "offset by hostname"
    This reverts commit ef9a0573957caeaa16bc986f54b0c161a0548881.

--- rediss3/recipes/default.rb
+++ rediss3/recipes/default.rb
@@ -10,7 +10,7 @@ include_recipe 'cron'
 
 cron_d 'redis-cron' do
   minute  "0"
-  hour    "*/4+#{node[:opsworks][:instance][:hostname].scan(/\d+/).first % 4}"
+  hour    "*/4"
   command "redis-cli bgsave && sleep 3600 && s3cmd --config /root/.s3cfg put #{node[:rediss3][:rdb_path]}/#{node[:rediss3][:rdb_filename]} s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}"
   user    "root"
   path    "/usr/local/bin"
