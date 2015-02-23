df27df9a7a95355ed0d42fb8874f76f685c81513
flrngel <flrngel@gmail.com>
2015-02-23 12:05:36 +0900

    save to bgsave

--- rediss3/recipes/default.rb
+++ rediss3/recipes/default.rb
@@ -11,7 +11,7 @@ include_recipe 'cron'
 cron_d 'redis-cron' do
   minute  "0"
   hour    "*/4"
-  command "redis-cli save && s3cmd --config /root/.s3cfg put #{node[:rediss3][:rdb_path]}/#{node[:rediss3][:rdb_filename]} s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}"
+  command "redis-cli bgsave && sleep 3600 && s3cmd --config /root/.s3cfg put #{node[:rediss3][:rdb_path]}/#{node[:rediss3][:rdb_filename]} s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}"
   user    "root"
   path    "/usr/local/bin"
 end
