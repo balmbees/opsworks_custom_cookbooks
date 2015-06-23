f5f8da9958e2ba3cf07a1b15f981913005c737cc
flrngel <flrngel@gmail.com>
2015-06-23 18:01:56 +0900

    Revert "change docker path"
    This reverts commit 4d85888383c0ce2424eb2d6b1d31e7d0d8134a49.

--- docker/recipes/install.rb
+++ docker/recipes/install.rb
@@ -2,8 +2,6 @@ case node[:platform]
 when "ubuntu","debian"
   execute "install docker" do
     command "curl -sSL https://get.docker.com/ubuntu/ | sudo sh"
-    command "export DOCKER_OPTS=' -g /data/docker -p /var/run/docker.pid'"
-    command "service docker restart"
   end
 when 'centos','redhat','fedora','amazon'
   package "docker" do
