a2bbe8f940fcde3cdc649cf5841a518108883830
flrngel <flrngel@gmail.com>
2015-06-24 10:16:47 +0900

    use /mnt and docker recommend script

--- docker/recipes/install.rb
+++ docker/recipes/install.rb
@@ -1,7 +1,7 @@
 case node[:platform]
 when "ubuntu","debian"
   execute "install docker" do
-    command "curl -sSL https://get.docker.com/ubuntu/ | sudo sh; export DOCKER_OPTS='-g /data/docker -p /var/run/docker.pid'; service docker restart"
+    command "curl -sSL https://get.docker.com/| sudo sh; export DOCKER_OPTS='-g /mnt/docker -p /var/run/docker.pid'; service docker restart"
   end
 when 'centos','redhat','fedora','amazon'
   package "docker" do
