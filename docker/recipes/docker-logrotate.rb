6d0556146333b61d66de78e4f82b27a854c9066d
flrngel <flrngel@gmail.com>
2015-07-22 17:07:05 +0900

    change logrotate configuration (missingok)

--- docker/recipes/docker-logrotate.rb
+++ docker/recipes/docker-logrotate.rb
@@ -6,14 +6,14 @@ logrotate_app "nginx" do
   path "/mnt/var/log/nginx/*.log"
   create "0644 www-data adm"
   frequency "daily"
-  options ["compress", "delaycompress", "sharedscripts", "notifempty"]
+  options ["compress", "delaycompress", "sharedscripts", "missingok"]
   prerotate <<-EOF
-    if [ -d /etc/logrotate.d/httpd-prerotate ]; then
+  if [ -d /etc/logrotate.d/httpd-prerotate ]; then
       run-parts /etc/logrotate.d/httpd-prerotate;
     fi
   EOF
   postrotate <<-EOF
-    sudo docker exec -it unicorn_rails sh -c 'cat /var/run/nginx.pid | xargs kill -USR1'
+  sudo docker exec -it unicorn_rails sh -c 'cat /var/run/nginx.pid | xargs kill -USR1'
     sudo docker exec -it unicorn_rails sh -c 'rm -rf /var/lib/logstash/.sincedb*'
   EOF
 end
