04c2c4510c4d8e9d7c303e1a51ae5ac199d15c9c
Charlie Jee <charlie@vingle.net>
2015-08-03 16:34:04 +0900

    FIx td-agent-checker again

--- td-agent-checker/templates/default/td.sh
+++ td-agent-checker/templates/default/td.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 session_file = Dir["/var/log/td-agent/buffer/td/session*"].first
-if File.mtime(session_file) > Time.now - 60*5 # updated in 5 mins
-  `aws cloudwatch put-metric-data --namespace TdAgent --metric-name HealthyAgent --value 1 --unit "Count"`
+if session_file and File.mtime(session_file) > Time.now - 60*5 # updated in 5 mins
+  `/usr/local/bin/aws cloudwatch put-metric-data --namespace TdAgent --metric-name HealthyAgent --value 1 --unit "Count"`
 else
-  `aws cloudwatch put-metric-data --namespace TdAgent --metric-name UnhealthyAgent --value 1 --unit "Count"`
+  `/usr/local/bin/aws cloudwatch put-metric-data --namespace TdAgent --metric-name UnhealthyAgent --value 1 --unit "Count"`
 end
