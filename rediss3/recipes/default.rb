65e85a3eec924d756a081e9c0152c6d5d9c282c9
flrngel <flrngel@gmail.com>
2015-08-03 11:59:38 +0900

    when redis restarts, restore rdb file from latest s3 backups

--- rediss3/recipes/default.rb
+++ rediss3/recipes/default.rb
@@ -24,3 +24,43 @@ cron_d 's3-cron' do
   path    "/usr/local/bin"
 end
 
+bash "restore from backups" do
+  user "root"
+  cwd "#{node[:rediss3][:rdb_path]}"
+
+  code <<-EOH
+    FFILENAME=#{node[:rediss3][:rdb_filename]}
+    FFILESIZE=$(stat -c %s "$FFILENAME")
+
+    for HTIME in {21,17,13,9,5,1}
+    do
+      FILENAME=#{node[:rediss3][:rdb_filename]}
+      FILESIZE=$(stat -c %s "$FILENAME")
+
+      if [ $FILESIZE -lt #{1024*1024*10} ]
+      then
+        /usr/local/bin/s3cmd --config /root/.s3cfg get s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}_#{Date.today.strftime}-$HTIME #{node[:rediss3][:rdb_filename]}
+      fi
+    done
+
+    for HTIME in {21,17,13,9,5,1}
+    do
+      FILENAME=#{node[:rediss3][:rdb_filename]}
+      FILESIZE=$(stat -c %s "$FILENAME")
+
+      if [ $FILESIZE -lt #{1024*1024*10} ]
+      then
+        /usr/local/bin/s3cmd --config /root/.s3cfg get s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}_#{Date.today.prev_day.strftime}-$HTIME #{node[:rediss3][:rdb_filename]}
+      fi
+    done
+
+    SFILENAME=#{node[:rediss3][:rdb_filename]}
+    SFILESIZE=$(stat -c %s "$SFILENAME")
+
+    if [ $SFILESIZE -ne $SFILESIZE ]
+    then
+      service redis6379 stop
+      service redis6379 start
+    fi
+  EOH
+end
