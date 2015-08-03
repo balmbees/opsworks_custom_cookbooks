3aa5f78ae5e3644dbb27564ebdaba36fcca5408a
flrngel <flrngel@gmail.com>
2015-08-03 16:06:16 +0900

    chef log test

--- rediss3/recipes/default.rb
+++ rediss3/recipes/default.rb
@@ -24,80 +24,46 @@ cron_d 's3-cron' do
   path    "/usr/local/bin"
 end
 
+restore_backup_script = <<-EOH
+  FFILENAME=#{node[:rediss3][:rdb_filename]}
+  FFILESIZE=$(stat -c %s "$FFILENAME")
+
+  for HTIME in {21,17,13,9,5,1}
+  do
+    FILENAME=#{node[:rediss3][:rdb_filename]}
+    FILESIZE=$(stat -c %s "$FILENAME")
+
+    if [ $FILESIZE -lt #{1024*1024*10} ]
+    then
+      /usr/local/bin/s3cmd --config /root/.s3cfg get s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}_#{Date.today.strftime}-$HTIME #{node[:rediss3][:rdb_filename]} --force
+    fi
+  done
+
+  for HTIME in {21,17,13,9,5,1}
+  do
+    FILENAME=#{node[:rediss3][:rdb_filename]}
+    FILESIZE=$(stat -c %s "$FILENAME")
+
+    if [ $FILESIZE -lt #{1024*1024*10} ]
+    then
+      /usr/local/bin/s3cmd --config /root/.s3cfg get s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}_#{Date.today.prev_day.strftime}-$HTIME #{node[:rediss3][:rdb_filename]} --force
+    fi
+  done
+
+  SFILENAME=#{node[:rediss3][:rdb_filename]}
+  SFILESIZE=$(stat -c %s "$SFILENAME")
+
+  if [ $SFILESIZE -ne $SFILESIZE ]
+  then
+    service redis6379 stop
+    service redis6379 start
+  fi
+EOH
+
 bash "restore from backups" do
   user "root"
   cwd "#{node[:rediss3][:rdb_path]}"
-
-  code <<-EOH
-    FFILENAME=#{node[:rediss3][:rdb_filename]}
-    FFILESIZE=$(stat -c %s "$FFILENAME")
-
-    for HTIME in {21,17,13,9,5,1}
-    do
-      FILENAME=#{node[:rediss3][:rdb_filename]}
-      FILESIZE=$(stat -c %s "$FILENAME")
-
-      if [ $FILESIZE -lt #{1024*1024*10} ]
-      then
-        /usr/local/bin/s3cmd --config /root/.s3cfg get s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}_#{Date.today.strftime}-$HTIME #{node[:rediss3][:rdb_filename]} --force
-      fi
-    done
-
-    for HTIME in {21,17,13,9,5,1}
-    do
-      FILENAME=#{node[:rediss3][:rdb_filename]}
-      FILESIZE=$(stat -c %s "$FILENAME")
-
-      if [ $FILESIZE -lt #{1024*1024*10} ]
-      then
-        /usr/local/bin/s3cmd --config /root/.s3cfg get s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}_#{Date.today.prev_day.strftime}-$HTIME #{node[:rediss3][:rdb_filename]} --force
-      fi
-    done
-
-    SFILENAME=#{node[:rediss3][:rdb_filename]}
-    SFILESIZE=$(stat -c %s "$SFILENAME")
-
-    if [ $SFILESIZE -ne $SFILESIZE ]
-    then
-      service redis6379 stop
-      service redis6379 start
-    fi
-  EOH
-  
-  Chef::Log.info(<<-EOH
-    FFILENAME=#{node[:rediss3][:rdb_filename]}
-    FFILESIZE=$(stat -c %s "$FFILENAME")
-
-    for HTIME in {21,17,13,9,5,1}
-    do
-      FILENAME=#{node[:rediss3][:rdb_filename]}
-      FILESIZE=$(stat -c %s "$FILENAME")
-
-      if [ $FILESIZE -lt #{1024*1024*10} ]
-      then
-        /usr/local/bin/s3cmd --config /root/.s3cfg get s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}_#{Date.today.strftime}-$(printf "%02s" "$HTIME") #{node[:rediss3][:rdb_filename]}
-      fi
-    done
-
-    for HTIME in {21,17,13,9,5,1}
-    do
-      FILENAME=#{node[:rediss3][:rdb_filename]}
-      FILESIZE=$(stat -c %s "$FILENAME")
-
-      if [ $FILESIZE -lt #{1024*1024*10} ]
-      then
-        /usr/local/bin/s3cmd --config /root/.s3cfg get s3://#{node[:rediss3][:s3_prefix]}/#{node[:opsworks][:instance][:hostname]}/#{node[:rediss3][:rdb_filename]}_#{Date.today.prev_day.strftime}-$(printf "%02s" "$HTIME") #{node[:rediss3][:rdb_filename]}
-      fi
-    done
-
-    SFILENAME=#{node[:rediss3][:rdb_filename]}
-    SFILESIZE=$(stat -c %s "$SFILENAME")
-
-    if [ $SFILESIZE -ne $SFILESIZE ]
-    then
-      service redis6379 stop
-      service redis6379 start
-    fi
-  EOH
-  )
+  code restore_backup_script
 end
+
+Chef::Log.info(restore_backup_script)
