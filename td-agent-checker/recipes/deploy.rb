2eb0028a07752faab118873ecc7ea8cd77d905af
Charlie Jee <charlie@vingle.net>
2015-08-04 11:55:08 +0900

    change user to execute crontab with different user

--- td-agent-checker/recipes/deploy.rb
+++ td-agent-checker/recipes/deploy.rb
@@ -2,16 +2,19 @@ include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
   template "/tmp/td.sh" do
+    user "ubuntu"
     source "td.sh"
     mode 0755
   end
 
   template "/etc/crontab.conf" do
+    user "ubuntu"
     mode "0644"
     source "crontab.conf.erb"
   end
 
   execute "setup crontab" do
+    user "ubuntu"
     command "crontab /etc/crontab.conf"
   end
 end
