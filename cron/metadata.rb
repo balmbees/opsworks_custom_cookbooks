bb07f333d896b14f131ebcb3333d63685bfe47de
Charlie Jee <charlie@vingle.net>
2015-08-28 10:47:31 +0900

    Add cron recipe

---
+++ cron/metadata.rb
@@ -0,0 +1,16 @@
+name 'cron'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs cron'
+
+version '1.6.1'
+
+recipe 'cron', 'Installs the cron package and starts the crond service.'
+
+%w(ubuntu debian fedora redhat centos scientific oracle amazon smartos omnios solaris2 gentoo).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/opscode-cookbooks/cron' if respond_to?(:source_url)
+issues_url 'https://github.com/opscode-cookbooks/cron/issues' if respond_to?(:source_url)
