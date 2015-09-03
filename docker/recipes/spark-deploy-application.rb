76945fb7cc61d483ec5e819e8b75e3a386478978
flrngel <flrngel@gmail.com>
2015-09-03 11:10:09 +0900

    change time

--- docker/recipes/spark-deploy-application.rb
+++ docker/recipes/spark-deploy-application.rb
@@ -15,6 +15,6 @@ end
 
 cron_d "soft_fandoms" do
   minute "5"
-  hour "1"
+  hour "0"
   command "docker exec spark bash -c 'spark-submit --master spark://#{node[:custom_env][:spark][:MASTER_IP]}:7077 --packages org.apache.hadoop:hadoop-aws:2.7.1 --conf spark.akka.frameSize=50 /spark_app/soft_fandoms.py'"
 end
