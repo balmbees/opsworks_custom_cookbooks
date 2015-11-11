29f0dd301b1e96de3a8a53b661f3c5725bf4a422
flrngel <flrngel@gmail.com>
2015-11-11 18:23:01 +0900

    no-enter

--- discovery/recipes/default.rb
+++ discovery/recipes/default.rb
@@ -1,13 +1,11 @@
 include_recipe 'cron'
 
 code = <<-CODE
-
 curl -X put -d '{ "Datacenter": "dc1", "Node": "#{node[:opsworks][:instance][:hostname]}", "Address": "#{node[:opsworks][:instance][:private_ip]}" }' http://107.21.109.230:8500/v1/catalog/register > /dev/null
-
 CODE
 
-
-cron_d "discovery service" do
+cron_d "discovery-service" do
   command code
   user "root"
+  path "/usr/local/bin"
 end
