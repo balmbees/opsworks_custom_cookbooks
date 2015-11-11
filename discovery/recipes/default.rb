b13fec5bcf22a0c80efafce6112be7c9b36699d6
flrngel <flrngel@gmail.com>
2015-11-11 16:45:55 +0900

    change nohup

--- discovery/recipes/default.rb
+++ discovery/recipes/default.rb
@@ -1,4 +1,4 @@
 bash "discovery service" do
   user "root"
-  code "nohup until [ -f 1 ]; do sleep 60; curl -X put -d '{ \"Datacenter\": \"dc1\", \"Node\": \"#{node[:opsworks][:instance][:hostname]}\", \"Address\": \"#{node[:opsworks][:instance][:private_ip]}\" }' http://107.21.109.230:8500/v1/catalog/register > /dev/null; done &"
+  code "nohup bash -c 'until [ -f 1 ]; do sleep 60; curl -X put -d \"{ \"Datacenter\": \"dc1\", \"Node\": \"#{node[:opsworks][:instance][:hostname]}\", \"Address\": \"#{node[:opsworks][:instance][:private_ip]}\" }\" http://107.21.109.230:8500/v1/catalog/register > /dev/null; done' &"
 end
