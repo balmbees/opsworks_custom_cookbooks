639ae2c558884bf9151aa025cfb3219de780a73b
flrngel <flrngel@gmail.com>
2015-11-11 18:06:28 +0900

    use cron

--- discovery/recipes/default.rb
+++ discovery/recipes/default.rb
@@ -1,4 +1,13 @@
-bash "discovery service" do
+include_recipe 'cron'
+
+code = <<-CODE
+
+curl -X put -d '{ "Datacenter": "dc1", "Node": "#{node[:opsworks][:instance][:hostname]}", "Address": "#{node[:opsworks][:instance][:private_ip]}" }' http://107.21.109.230:8500/v1/catalog/register > /dev/null
+
+CODE
+
+
+cron_d "discovery service" do
+  command code
   user "root"
-  code "nohup bash -c 'until [ -f 1 ]; do sleep 60; curl -X put -d \"{ \\'Datacenter\\': \\'dc1\\', \\'Node\\': \\'#{node[:opsworks][:instance][:hostname]}\\', \\'Address\\': \\'#{node[:opsworks][:instance][:private_ip]}\\' }\" http://107.21.109.230:8500/v1/catalog/register > /dev/null; done' &"
 end
