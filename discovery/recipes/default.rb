5a2ccebba6b3095abadb33316c445e9104c24de1
flrngel <flrngel@gmail.com>
2015-11-13 11:12:42 +0900

    PATH env

--- discovery/recipes/default.rb
+++ discovery/recipes/default.rb
@@ -1,11 +1,11 @@
 include_recipe 'cron'
 
 code = <<-CODE
-curl --silent -X put -d '{ "Datacenter": "dc1", "Node": "#{node[:opsworks][:instance][:hostname]}", "Address": "#{node[:opsworks][:instance][:private_ip]}" }' http://107.21.109.230:8500/v1/catalog/register 2&1> /dev/null
+curl -s -X put -d '{ "Datacenter": "dc1", "Node": "#{node[:opsworks][:instance][:hostname]}", "Address": "#{node[:opsworks][:instance][:private_ip]}" }' http://107.21.109.230:8500/v1/catalog/registe 2>&1 > /dev/null
 CODE
 
 cron_d "discovery-service" do
   command code
   user "root"
-  path "/usr/local/bin"
+  path "/usr/local/bin:/usr/bin"
 end
