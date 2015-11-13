174b66540539cb3bc57ec1cbf61127f976587deb
flrngel <flrngel@gmail.com>
2015-11-13 11:26:47 +0900

    r

--- discovery/recipes/default.rb
+++ discovery/recipes/default.rb
@@ -1,7 +1,7 @@
 include_recipe 'cron'
 
 code = <<-CODE
-curl -s -X put -d '{ "Datacenter": "dc1", "Node": "#{node[:opsworks][:instance][:hostname]}", "Address": "#{node[:opsworks][:instance][:private_ip]}" }' http://107.21.109.230:8500/v1/catalog/registe 2>&1 > /dev/null
+curl -s -X put -d '{ "Datacenter": "dc1", "Node": "#{node[:opsworks][:instance][:hostname]}", "Address": "#{node[:opsworks][:instance][:private_ip]}" }' http://107.21.109.230:8500/v1/catalog/register 2>&1 > /dev/null
 CODE
 
 cron_d "discovery-service" do
