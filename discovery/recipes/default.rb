9c3f8a3e3d83daac156d30b862654160b73f8e8c
flrngel <flrngel@gmail.com>
2015-11-13 10:32:52 +0900

    silent hill

--- discovery/recipes/default.rb
+++ discovery/recipes/default.rb
@@ -1,7 +1,7 @@
 include_recipe 'cron'
 
 code = <<-CODE
-curl -X put -d '{ "Datacenter": "dc1", "Node": "#{node[:opsworks][:instance][:hostname]}", "Address": "#{node[:opsworks][:instance][:private_ip]}" }' http://107.21.109.230:8500/v1/catalog/register > /dev/null
+curl --silent -X put -d '{ "Datacenter": "dc1", "Node": "#{node[:opsworks][:instance][:hostname]}", "Address": "#{node[:opsworks][:instance][:private_ip]}" }' http://107.21.109.230:8500/v1/catalog/register 2&1> /dev/null
 CODE
 
 cron_d "discovery-service" do
