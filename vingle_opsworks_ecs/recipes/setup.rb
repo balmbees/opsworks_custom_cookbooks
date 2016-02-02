8c01b6bc140723fb3abdcba69b6641b1e7f592c5
juyeong <juyeong9@gmail.com>
2016-02-02 20:21:25 +0900

    restart ecs agent after setup

--- vingle_opsworks_ecs/recipes/setup.rb
+++ vingle_opsworks_ecs/recipes/setup.rb
@@ -45,3 +45,39 @@ bash "Login dockerhub ecs" do
     echo 'ECS_ENGINE_AUTH_DATA={"https://index.docker.io/v1/":{"username":"#{node[:custom_env][:vingle][:DOCKER_USERNAME]}","password":"#{node[:custom_env][:vingle][:DOCKER_PASSWORD]}","email":"#{node[:custom_env][:vingle][:DOCKER_EMAIL]}"}}' >> /etc/ecs/ecs.config
   EOH
 end
+
+execute "Stop exsiting the Amazon ECS agent" do
+  command ["/usr/bin/docker",
+           "rm",
+           "-f",
+           "ecs-agent"].join(" ")
+
+  only_if do
+    ::File.exist?("/usr/bin/docker") && OpsWorks::ShellOut.shellout("docker ps -a").include?("amazon-ecs-agent")
+  end
+end
+
+execute "Start the Amazon ECS agent" do
+  command ["/usr/bin/docker",
+           "run",
+           "--name ecs-agent",
+           "-d",
+           "-v /var/run/docker.sock:/var/run/docker.sock",
+           "-v /var/log/ecs:/log",
+           "-v /var/lib/ecs/data:/data",
+           "-p 127.0.0.1:51678:51678",
+           "--env-file /etc/ecs/ecs.config",
+           "amazon/amazon-ecs-agent:latest"].join(" ")
+
+  only_if do
+    ::File.exist?("/usr/bin/docker") && !OpsWorks::ShellOut.shellout("docker ps -a").include?("amazon-ecs-agent")
+  end
+end
+
+ruby_block "Check that the ECS agent is running" do
+  block do
+    ecs_agent = OpsWorks::ECSAgent.new
+
+    Chef::Application.fatal!("ECS agent could not start.") unless ecs_agent.wait_for_availability
+  end
+end
