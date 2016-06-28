e45c65bf2ba8b69d5b88d845e53146f96ae12d9d
JeongHoon Baek <jude@vingle.net>
2016-06-28 15:08:55 +0900

    Separate plugin versions from recipe to attributes: for upgrading

---
+++ elasticsearch/attributes/text_plugins.rb
@@ -0,0 +1,7 @@
+default[:elasticsearch][:plugin] = {
+  "elasticsearch-analysis-icu" => "2.4.3",
+  "elasticsearch-analysis-smartcn" => "2.4.4",
+  "elasticsearch-analysis-kuromoji" => "2.4.3",
+  "elasticsearch-cloud-aws" => "2.4.2",
+  "pg-jdbc-driver" => "postgresql-9.4.1208.jre7.jar"
+}
--- elasticsearch/recipes/text_plugins.rb
+++ elasticsearch/recipes/text_plugins.rb
@@ -2,11 +2,18 @@ script "install_plugin_es" do
   interpreter "bash"
   user "root"
   cwd "/usr/share/elasticsearch/"
+
+  # only compatible until 1.5.0 : must rewrite after 1.5.2
   code <<-EOH
-  bin/plugin -i elasticsearch/elasticsearch-analysis-icu/2.4.3
-  bin/plugin -i elasticsearch/elasticsearch-analysis-smartcn/2.4.4
-  bin/plugin -i elasticsearch/elasticsearch-analysis-kuromoji/2.4.3
+  bin/plugin -i mobz/elasticsearch-head
+
+  bin/plugin -i elasticsearch/elasticsearch-analysis-icu/#{node[:elasticsearch][:plugin]['elasticsearch-analysis-icu']}
+  bin/plugin -i elasticsearch/elasticsearch-analysis-smartcn/#{node[:elasticsearch][:plugin]['elasticsearch-analysis-smartcn']}
+  bin/plugin -i elasticsearch/elasticsearch-analysis-kuromoji/#{node[:elasticsearch][:plugin]['elasticsearch-analysis-kuromoji']}
+  bin/plugin -i elasticsearch/elasticsearch-cloud-aws/#{node[:elasticsearch][:plugin]['elasticsearch-cloud-aws']}
+
   bin/plugin -i analysis-mecab-ko-0.16.3 -u https://bitbucket.org/eunjeon/mecab-ko-lucene-analyzer/downloads/elasticsearch-analysis-mecab-ko-0.16.3.zip
+
   wget https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-1.6.1-20140814.tar.gz
   tar xvzf mecab-ko-dic-1.6.1-20140814.tar.gz
   cd mecab-ko-dic-1.6.1-20140814/
@@ -15,12 +22,9 @@ script "install_plugin_es" do
   make && make install
   cd ..
 
-  bin/plugin -i jdbc -u http://xbib.org/repository/org/xbib/elasticsearch/plugin/elasticsearch-river-jdbc/1.4.0.8/elasticsearch-river-jdbc-1.4.0.8-plugin.zip
-  wget https://jdbc.postgresql.org/download/postgresql-9.3-1103.jdbc41.jar
-  cp postgresql-9.3-1103.jdbc41.jar /usr/share/elasticsearch/plugins/jdbc/
-
-  bin/plugin -i mobz/elasticsearch-head
-  bin/plugin -i elasticsearch/elasticsearch-cloud-aws/2.4.2
+  bin/plugin -i jdbc -u http://xbib.org/repository/org/xbib/elasticsearch/plugin/elasticsearch-river-jdbc/1.4.4.5/elasticsearch-river-jdbc-1.4.4.5-plugin.zip
+  wget https://jdbc.postgresql.org/download/#{node[:elasticsearch][:plugin]['pg-jdbc-driver']}
+  mv #{node[:elasticsearch][:plugin]['pg-jdbc-driver']} /usr/share/elasticsearch/plugins/jdbc/
   EOH
   not_if { File.exist?("/usr/share/elasticsearch/plugins/head") }
 end
