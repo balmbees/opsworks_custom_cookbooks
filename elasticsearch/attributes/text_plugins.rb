168d47a084cf6797b76a7d3eca1a4451bcc99844
JeongHoon Baek <jude@vingle.net>
2016-06-29 15:20:22 +0900

    Fix: install plugin & dictionary

--- elasticsearch/attributes/text_plugins.rb
+++ elasticsearch/attributes/text_plugins.rb
@@ -3,5 +3,6 @@ default[:elasticsearch][:plugin] = {
   "elasticsearch-analysis-smartcn" => "2.7.0",
   "elasticsearch-analysis-kuromoji" => "2.7.0",
   "elasticsearch-cloud-aws" => "2.7.1",
-  "elasticsearch-jdbc" => "1.7.3.0"
+  "elasticsearch-jdbc" => "1.7.3.0",
+  "elasticsearch-mecab-ko" => "1.7.3.0"
 }
--- elasticsearch/recipes/text_plugins.rb
+++ elasticsearch/recipes/text_plugins.rb
@@ -5,30 +5,36 @@ script "install_plugin_es" do
 
   if node[:elasticsearch][:version].to_f >= 2.0
     code_plugins = <<-EOH
+      bin/plugin install mobz/elasticsearch-head
+
       bin/plugin install analysis-icu
       bin/plugin install analysis-smartcn
       bin/plugin install analysis-kuromoji
       bin/plugin install cloud-aws
+      bin/plugin install https://bitbucket.org/eunjeon/mecab-ko-lucene-analyzer/downloads/elasticsearch-analysis-mecab-ko-#{node[:elasticsearch][:plugin]['elasticsearch-mecab-ko']}.zip
+      wget https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.0.1-20150920.tar.gz
+      tar xvzf mecab-ko-dic-2.0.1-20150920.tar.gz
+      cd mecab-ko-dic-2.0.1-20150920/
     EOH
   else
     code_plugins = <<-EOH
+      bin/plugin -i mobz/elasticsearch-head
+
       bin/plugin -i elasticsearch/elasticsearch-analysis-icu/#{node[:elasticsearch][:plugin]['elasticsearch-analysis-icu']}
       bin/plugin -i elasticsearch/elasticsearch-analysis-smartcn/#{node[:elasticsearch][:plugin]['elasticsearch-analysis-smartcn']}
       bin/plugin -i elasticsearch/elasticsearch-analysis-kuromoji/#{node[:elasticsearch][:plugin]['elasticsearch-analysis-kuromoji']}
       bin/plugin -i elasticsearch/elasticsearch-cloud-aws/#{node[:elasticsearch][:plugin]['elasticsearch-cloud-aws']}
+      bin/plugin -i analysis-mecab-ko-0.16.3 -u https://bitbucket.org/eunjeon/mecab-ko-lucene-analyzer/downloads/elasticsearch-analysis-mecab-ko-0.16.3.zip
+
+      wget https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-1.6.1-20140814.tar.gz
+      tar xvzf mecab-ko-dic-1.6.1-20140814.tar.gz
+      cd mecab-ko-dic-1.6.1-20140814/
     EOH
   end
 
   code <<-EOH
-  bin/plugin -i mobz/elasticsearch-head
-
   #{code_plugins}
 
-  bin/plugin -i analysis-mecab-ko-0.16.3 -u https://bitbucket.org/eunjeon/mecab-ko-lucene-analyzer/downloads/elasticsearch-analysis-mecab-ko-0.16.3.zip
-
-  wget https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-1.6.1-20140814.tar.gz
-  tar xvzf mecab-ko-dic-1.6.1-20140814.tar.gz
-  cd mecab-ko-dic-1.6.1-20140814/
   ./autogen.sh
   ./configure
   make && make install
