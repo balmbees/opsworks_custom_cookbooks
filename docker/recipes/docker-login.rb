a71cfcf089027d330dfa68cb25933da760a97b98
juyeong <juyeong9@gmail.com>
2016-07-12 10:11:02 +0900

    fix docker login

--- docker/recipes/docker-login.rb
+++ docker/recipes/docker-login.rb
@@ -1,7 +1,6 @@
 Chef::Log.info("docker login")
 bash "docker login" do
   user "root"
-  cwd "#{deploy[:deploy_to]}"
   code <<-EOH
     docker login -e #{node[:custom_env][:vingle][:DOCKER_EMAIL]} -u #{node[:custom_env][:vingle][:DOCKER_USERNAME]} -p #{node[:custom_env][:vingle][:DOCKER_PASSWORD]}
   EOH
