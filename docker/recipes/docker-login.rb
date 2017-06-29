99268e896fe8a1c7444624d5c5b9c70a85a31083
MooYeol Prescott Lee <mooyoul@gmail.com>
2017-06-29 17:28:24 +0900

    remove deprecated email args from docker-login script
    - Ref: https://github.com/docker/cli/pull/143

--- docker/recipes/docker-login.rb
+++ docker/recipes/docker-login.rb
@@ -2,6 +2,6 @@ Chef::Log.info("docker login")
 bash "docker login" do
   user "root"
   code <<-EOH
-    docker login -e #{node[:custom_env][:vingle][:DOCKER_EMAIL]} -u #{node[:custom_env][:vingle][:DOCKER_USERNAME]} -p #{node[:custom_env][:vingle][:DOCKER_PASSWORD]}
+    docker login -u #{node[:custom_env][:vingle][:DOCKER_USERNAME]} -p #{node[:custom_env][:vingle][:DOCKER_PASSWORD]}
   EOH
 end
